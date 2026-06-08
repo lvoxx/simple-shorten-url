58d07208b58b18d39c0cbce7780b048bf26ab6f7
lvoxx <lvoxxartist@gmail.com>
2026-06-08 14:34:37 +0700

    Deleted mock-api

--- mock-api/mock.ts
+++
@@ -1,8 +0,0 @@
-import { server } from './mocks/server'
-
-server.listen()
-
-console.log('Mock server running...')
-
-// Keep connection stay alive
-setInterval(() => {}, 8080)
---
+++ shorten-url-app/.output/server/chunks/build/styles.mjs
@@ -0,0 +1,12 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/welcome.vue": () => import('./welcome-styles.CWvdbD1w.mjs').then(interopDefault),
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/welcome.vue?vue&type=style&index=0&scoped=5af44403&lang.css": () => import('./welcome-styles.CWvdbD1w.mjs').then(interopDefault),
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.Gw9WXElQ.mjs').then(interopDefault),
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.CUjbObP8.mjs').then(interopDefault),
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=261e4132&lang.css": () => import('./error-404-styles.Gw9WXElQ.mjs').then(interopDefault),
+  "../node_modules/.pnpm/nuxt@4.4.5_@babel+core@7.29_a30946f8c1912b503f6744f0fbdcfe2a/node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=20c41cc3&lang.css": () => import('./error-500-styles.CUjbObP8.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
